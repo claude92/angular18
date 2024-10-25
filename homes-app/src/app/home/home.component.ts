d20dc1f463ccfa90069c12821cbadfe119379f87
claude92 <claudiu.mocanu@live.com>
2024-10-25 15:24:37 +0200

    4 interface

--- homes-app/src/app/home/home.component.ts
+++ homes-app/src/app/home/home.component.ts
@@ -1,3 +1,4 @@
+import { HousingLocation } from "./../housinglocation";
 import { Component } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { HousingLocationComponent } from "../housing-location/housing-location.component";
@@ -18,4 +19,16 @@ import { HousingLocationComponent } from "../housing-location/housing-location.c
   `,
   styleUrls: ["./home.component.css"],
 })
-export class HomeComponent {}
+export class HomeComponent {
+  readonly baseUrl = "https://angular.dev/assets/images/tutorials/common";
+  housingLocation: HousingLocation = {
+    id: 9999,
+    name: "Test Home",
+    city: "Test city",
+    state: "ST",
+    photo: `${this.baseUrl}/example-house.jpg`,
+    availableUnits: 99,
+    wifi: true,
+    laundry: false,
+  };
+}
