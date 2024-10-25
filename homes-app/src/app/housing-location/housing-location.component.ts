77e402a9cae01e2bbb1b80b1f6ca4c99220fd862
claude92 <claudiu.mocanu@live.com>
2024-10-25 16:50:59 +0200

    add property binding to components

--- homes-app/src/app/housing-location/housing-location.component.ts
+++ homes-app/src/app/housing-location/housing-location.component.ts
@@ -1,17 +1,13 @@
-import { Component } from '@angular/core';
-import { CommonModule } from '@angular/common';
-
+import { HousingLocation } from "./../housinglocation";
+import { Component, Input } from "@angular/core";
+import { CommonModule } from "@angular/common";
 @Component({
-  selector: 'app-housing-location',
+  selector: "app-housing-location",
   standalone: true,
   imports: [CommonModule],
-  template: `
-    <p>
-      housing-location works!
-    </p>
-  `,
-  styleUrls: ['./housing-location.component.css']
+  template: ` <p>housing-location works!</p> `,
+  styleUrls: ["./housing-location.component.css"],
 })
 export class HousingLocationComponent {
-
+  @Input() housingLocation!: HousingLocation; //The exclamation point is called the non-null assertion operator and it tells the TypeScript compiler that the value of this property won't be null or undefined
 }
